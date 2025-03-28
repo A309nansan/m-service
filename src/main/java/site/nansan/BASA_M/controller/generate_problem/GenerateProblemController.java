52173c7439bbe1a975dd108313933b6490bf62c5
전성호 <kopasd99@gmail.com>
2025-03-28 16:28:20 +0900

    [FEAT] : Problem Generate 에서 에러 핸들링 추가

--- src/main/java/site/nansan/BASA_M/controller/generate_problem/GenerateProblemController.java
+++ src/main/java/site/nansan/BASA_M/controller/generate_problem/GenerateProblemController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import site.nansan.BASA_M.dto.GeneratedProblemResponse;
+import site.nansan.BASA_M.global.exception.ProblemGenerationErrorCode;
+import site.nansan.BASA_M.global.exception.ProblemGenerationException;
 import site.nansan.BASA_M.service.generate_problem.*;
 
 @RestController
@@ -25,35 +27,35 @@ public class GenerateProblemController implements GenerateProblemSwaggerControll
                 case 2 -> m1ProblemGenerationService.generateM1_2Problem();
                 case 3 -> m1ProblemGenerationService.generateM1_3Problem();
                 case 4 -> m1ProblemGenerationService.generateM1_4Problem();
-                default -> throw new IllegalArgumentException("Invalid child parameter for group 1");
+                default -> throw new ProblemGenerationException(ProblemGenerationErrorCode.MEMBER_VALIDATION_COUNT_FAILED);
             };
             case 2 -> switch (child) {
                 case 1 -> m2ProblemGenerationService.generateM2_1Problem();
                 case 2 -> m2ProblemGenerationService.generateM2_2Problem();
                 case 3 -> m2ProblemGenerationService.generateM2_3Problem();
                 case 4 -> m2ProblemGenerationService.generateM2_4Problem();
-                default -> throw new IllegalArgumentException("Invalid child parameter for group 2");
+                default -> throw new ProblemGenerationException(ProblemGenerationErrorCode.MEMBER_VALIDATION_COUNT_FAILED);
             };
             case 3 -> switch (child) {
                 case 1 -> m3ProblemGenerationService.generateM3_1Problem();
                 case 2 -> m3ProblemGenerationService.generateM3_2Problem();
                 case 3 -> m3ProblemGenerationService.generateM3_3Problem();
                 case 4 -> m3ProblemGenerationService.generateM3_4Problem();
-                default -> throw new IllegalArgumentException("Invalid child parameter for group 3");
+                default -> throw new ProblemGenerationException(ProblemGenerationErrorCode.MEMBER_VALIDATION_COUNT_FAILED);
             };
             case 4 -> switch (child) {
                 case 1 -> m4ProblemGenerationService.generateM4_1Problem();
                 case 2 -> m4ProblemGenerationService.generateM4_2Problem();
                 case 3 -> m4ProblemGenerationService.generateM4_3Problem();
                 case 4 -> m4ProblemGenerationService.generateM4_4Problem();
-                default -> throw new IllegalArgumentException("Invalid child parameter for group 4");
+                default -> throw new ProblemGenerationException(ProblemGenerationErrorCode.MEMBER_VALIDATION_COUNT_FAILED);
             };
             case 5 -> switch (child) {
                 case 1 -> m5ProblemGenerationService.generateM5_1Problem();
                 case 2 -> m5ProblemGenerationService.generateM5_2Problem();
                 case 3 -> m5ProblemGenerationService.generateM5_3Problem();
                 case 4 -> m5ProblemGenerationService.generateM5_4Problem();
-                default -> throw new IllegalArgumentException("Invalid child parameter for group 5");
+                default -> throw new ProblemGenerationException(ProblemGenerationErrorCode.MEMBER_VALIDATION_COUNT_FAILED);
             };
             case 6 -> switch (child) {
                 case 1 -> m6ProblemGenerationService.generateM6_1Problem();
@@ -62,9 +64,9 @@ public class GenerateProblemController implements GenerateProblemSwaggerControll
                 case 4 -> m6ProblemGenerationService.generateM6_4Problem();
                 case 5 -> m6ProblemGenerationService.generateM6_5Problem();
                 case 6 -> m6ProblemGenerationService.generateM6_6Problem();
-                default -> throw new IllegalArgumentException("Invalid child parameter for group 6");
+                default -> throw new ProblemGenerationException(ProblemGenerationErrorCode.MEMBER_VALIDATION_COUNT_FAILED);
             };
-            default -> throw new IllegalArgumentException("Invalid group parameter");
+            default -> throw new ProblemGenerationException(ProblemGenerationErrorCode.MEMBER_VALIDATION_COUNT_FAILED);
         };
         return ResponseEntity.ok(response);
     }
--- src/main/java/site/nansan/BASA_M/global/exception/ProblemGenerationErrorCode.java
+++ src/main/java/site/nansan/BASA_M/global/exception/ProblemGenerationErrorCode.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ProblemGenerationErrorCode implements ErrorCode{
-    MEMBER_VALIDATION_COUNT_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "Util_400_1", "참여 인원 수는 0~255 사이여야 합니다."),
-    TEAM_VALIDATION_COUNT_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "Util_400_2", "참여 팀 수는 0~255 사이여야 합니다."),
-    SEQUENCE_VALIDATION_COUNT_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "Util_400_3", "스프린트 수는 0~255 사이여야 합니다."),
-    STATUS_VALIDATION_COUNT_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "Util_400_4", "상태 수는 0~3 사이여야 합니다.")
+    MEMBER_VALIDATION_COUNT_FAILED(HttpStatus.BAD_REQUEST, "Problem_400_1", "요청하신 문제 유형은 존재하지 않습니다."),
+    INVALID_N_VALUE(HttpStatus.BAD_REQUEST, "RANDOMUTIL_400_1", "n 자리수를 생성할때 n은 양수여야 합니다."),
+    INVALID_UPPER_BOUND(HttpStatus.BAD_REQUEST, "RANDOMUTIL_400_2", "upperBound 값은 지정된 n자리수 범위 내에 있어야 합니다."),
+    INVALID_LOWER_BOUND(HttpStatus.BAD_REQUEST, "RANDOMUTIL_400_3", "lowerBound는 upperBound보다 작거나 같아야 합니다.");
     ;
     private final HttpStatus status;
     private final String code;
