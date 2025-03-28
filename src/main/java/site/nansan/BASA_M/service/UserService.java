2cba61c333dbab7b09653600a6f511584b995550
전성호 <kopasd99@gmail.com>
2025-03-28 16:10:03 +0900

    [FEAT] : 요청 헤더로부터 userId 가져오는 로직 추가

---
+++ src/main/java/site/nansan/BASA_M/service/UserService.java
@@ -0,0 +1,21 @@
+package site.nansan.BASA_M.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final HttpServletRequest request;
+
+    public Long getAuthenticatedUserId() {
+        String userIdHeader = request.getHeader("X-User-Id");
+
+        if (userIdHeader == null) {
+            return null;
+        }
+        return Long.valueOf(userIdHeader);
+    }
+
+}
