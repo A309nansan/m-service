ac528870aa6ab2f4facb9e77510de4520147722e
전성호 <kopasd99@gmail.com>
2025-03-28 13:09:52 +0900

    [CONFIG] : 배포 설정

--- src/main/java/site/nansan/BASA_M/global/config/SwaggerConfig.java
+++ src/main/java/site/nansan/BASA_M/global/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI MServiceOpenAPI() {
+    public OpenAPI EnServiceOpenAPI() {
 
         return new OpenAPI()
                 .servers(List.of(
